5cdbf6d09de4a33a5c7c92194497f2cc3172bf04
Aubrey_Javachip <aschelba@ucsc.edu>
2021-05-04 23:50:53 -0700

    Main Info

--- src/main.js
+++ src/main.js
@@ -1,3 +1,22 @@
+<head>
+    Creators: Finn Morrison, Emersen Lorenz, Marla De Leon, Aubrey Schelbauer
+    Title: "Desert Ninja"
+    Completed: 5/4/21
+    Creative Tilt: 
+    (Technical)
+    We incorportaed and calculated for our game to have an increasing difficulty
+    based on the amount of points the player accumulates over time using a bell curve. 
+    This system will determine how many of each type of enemy will spawn as the game progresses,
+    for example, less points means easier enemies are more likely to spawn, while a lot of points will increase the chances of harder enemies to spawn.
+    We were also able to implement state machines for both the player actions and enemy actions, allowing us use state changes for every important event.
+    (Visual)
+    Tweens were added to make the enemies bob up and down to make them look like they were hovering in the air,
+    adding more to asethic of the game. The paralx scrolling also increases its speed the further the player progresses and the more points they earn. 
+    We are very proud of our custom designed soundtrack, sfx and animations created to make the game feel cohesive and visual interesting.
+    This was the first time for most of us creating pixel art so we are proud of what we were able to learn and accomplish for this project.
+</head>
+
+
 import Menu from "./scenes/Menu.js";
 import Instructions from "./scenes/Instructions.js";
 import Credits from "./scenes/Credits.js";
